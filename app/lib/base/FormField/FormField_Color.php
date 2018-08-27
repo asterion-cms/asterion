e26d3f666d32284e4a2ff5f9cdaee561652b7d02
Leano Martinet <theylooksotired@gmail.com>
2018-08-27 15:12:43 +0200

    Version 4.0

---
+++ app/lib/base/FormField/FormField_Color.php
@@ -0,0 +1,28 @@
+<?php
+/**
+* @class FormFieldColor
+*
+* This is a helper class to generate a color text form field.
+*
+* @author Leano Martinet <info@asterion-cms.com>
+* @package Asterion
+* @version 3.0.1
+*/
+class FormField_Color extends FormField_Default {
+
+    /**
+    * The constructor of the object.
+    */
+    public function __construct($options) {
+        parent::__construct($options);
+    }
+
+    /**
+    * Render the element with an static function.
+    */
+    static public function create($options) {
+        return FormField_Default::create($options);
+    }
+
+}
+?>
